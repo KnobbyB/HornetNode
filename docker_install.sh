d6c3d7f7d6624399289278ab185e14dc7d593a2f
KnobbyB <35898598+KnobbyB@users.noreply.github.com>
2020-04-08 22:31:08 -0700

    Add files via upload

---
+++ docker_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install \
+  nano \
+  git \
+  wget \	
+  screen
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce
+
+
+
+
+  
