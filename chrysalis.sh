ce386b2eaffe7c84c6e3acb5d74f063c1981839a
KnobbyB <35898598+KnobbyB@users.noreply.github.com>
2021-04-29 16:47:12 -0700

    Delete chrysalis.sh

--- chrysalis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update && apt upgrade -y
-wget -qO - https://ppa.hornet.zone/pubkey.txt | sudo apt-key add -
-sudo sh -c 'echo "deb http://ppa.hornet.zone stable main" >> /etc/apt/sources.list.d/hornet.list'
-sudo apt update
-sudo apt install hornet -y
-sudo systemctl enable hornet.service
-cd /var/lib/hornet
